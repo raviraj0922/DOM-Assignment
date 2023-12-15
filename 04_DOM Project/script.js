bb37bf0c7783c5002e777d5c5c64d4746e6d62f7
Ravi Raj Singh <134828819+raviraj0922@users.noreply.github.com>
2023-12-15 22:34:28 +0530

    project file uploaded

---
+++ 04_DOM Project/script.js
@@ -0,0 +1,34 @@
+//  The Barbarian
+let bgDiv = document.querySelector(".clash-card__unit-stats--barbarian")
+    bgDiv.setAttribute("style", "background-color: #ec9b3b; color: #ffffff")
+    
+    const innerDiv = document.querySelector(".clash-card__unit-stats--barbarian div:nth-child(3)");
+    innerDiv.setAttribute("style", "color: #ffffff")
+
+// The Archer
+let secDiv = document.querySelector(".clash-card__unit-stats--archer")
+    secDiv.setAttribute("style", "background-color: #ee5487; color: #ffffff")
+    
+    const secinnerDiv = document.querySelector(".clash-card__unit-stats--archer div:nth-child(3)");
+    secinnerDiv.setAttribute("style", "color: #ffffff")
+
+// The Giant
+let thrDiv = document.querySelector(".clash-card__unit-stats--giant")
+    thrDiv.setAttribute("style", "background-color: #f6901a; color: #ffffff")
+    
+    const thrinnerDiv = document.querySelector(".clash-card__unit-stats--giant div:nth-child(3)");
+    thrinnerDiv.setAttribute("style", "color: #ffffff")
+
+// The Goblin
+let fouDiv = document.querySelector(".clash-card__unit-stats--goblin")
+    fouDiv.setAttribute("style", "background-color: #82bb30; color: #ffffff")
+    
+    const fouinnerDiv = document.querySelector(".clash-card__unit-stats--goblin div:nth-child(3)");
+    fouinnerDiv.setAttribute("style", "color: #ffffff")
+
+// The Wizard
+let fivDiv = document.querySelector(".clash-card__unit-stats--wizard")
+    fivDiv.setAttribute("style", "background-color: #4facff; color: #ffffff")
+    
+    const fivinnerDiv = document.querySelector(".clash-card__unit-stats--wizard div:nth-child(3)");
+    fivinnerDiv.setAttribute("style", "color: #ffffff")
